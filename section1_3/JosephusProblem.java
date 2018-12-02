5ff78129cd1852625046965cd1b6c51571d6845a
totuet <tomvdberg@hotmail.com>
2018-12-02 15:57:17 +0100

    update

--- section1_3/JosephusProblem.java
+++ section1_3/JosephusProblem.java
@@ -17,64 +17,7 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
-public class JosephusProblem<Item> implements Iterable<Item>{
-    private Node first;
-    private Node last;
-    private int N;
-
-    private class Node {
-        Node next;
-        Item item;
-    }
-
-    public boolean isEmpty() {
-        return N == 0;
-    }
-
-    public int size() {
-        return N;
-    }
-
-    public void enqueu (Item item) {
-        Node oldlast = last;
-        last = new Node();
-        last.item = item;
-        last.next = null;
-        if (isEmpty())  first = last;
-        else            oldlast.next = last;
-        N++;
-
-    }
-
-    public Item dequeu() {
-        Item item = first.item;
-        first = first.next;
-        if (isEmpty()) last = null;
-        N--;
-        return item;
-    }
-
-    @Override
-    public Iterator<Item> iterator() {
-        return new ListIterator();
-    }
-
-    private class ListIterator implements Iterator<Item> {
-        private Node current = first;
-
-        public boolean hasNext() {
-            return current != null;
-        }
-
-        public void remove() { }
-
-        public Item next() {
-            Item item = current.item;
-            current = current.next;
-            return item;
-        }
-    }
-
+public class JosephusProblem{
     public static void main(String[] args) {
         Queue<Integer> que = new Queue<>();
         int numberOfPeople = 7;
